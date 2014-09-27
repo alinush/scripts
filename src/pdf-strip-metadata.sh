75636905bcf13c5c33491986411de0ce5a4bb3ed
Alin Tomescu <alinush@mit.edu>
2014-09-27 17:10:33 -0400

    added script to strip metadata from PDFs

---
+++ src/pdf-strip-metadata.sh
@@ -0,0 +1,31 @@
+set -e
+
+pdftk_strip_metadata() {
+   pdftk $1 dump_data | \
+   sed -e 's/\(InfoValue:\)\s.*/\1\ /g' | \
+   pdftk $1 update_info - output clean-$1 
+}
+
+qpdf_strip_metadata() {
+    qpdf --empty --pages $1 1-z -- $2
+}
+
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <pdf-file>"
+    exit 1
+fi
+
+if [ ! -f "$1" ]; then
+    echo "ERROR: '$1' is not a file"
+    exit 1
+fi 
+
+vOutFile="$1.stripped.pdf"
+
+echo "Writing stripped file to '$vOutFile' ..."
+qpdf_strip_metadata "$1" "$vOutFile"
+
+echo "New metadata (after stripping):"
+echo "------"
+
+pdftk "$vOutFile" dump_data
