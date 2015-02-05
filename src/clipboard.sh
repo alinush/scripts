c96085a5e3b1e3fd64e06a68b418b050c682e506
Alin Tomescu <alinush@mit.edu>
2015-02-05 16:51:17 -0500

    added clipboard tool

---
+++ src/clipboard.sh
@@ -0,0 +1,46 @@
+scriptdir=$(readlink -f $(dirname $0))
+
+vConfFile=$scriptdir/clipboard.conf
+
+if [ -z "$1" ]; then
+    echo "Usage: $(basename $0) <grep-pattern>"
+    exit 1
+fi
+
+if [ ! -f "$vConfFile" ]; then
+    echo "ERROR: No config file at '$vConfFile'"
+    exit 1
+fi
+
+vNumLines=`wc -l $vConfFile | cut -f 1 -d' '` 
+
+if [ $vNumLines != 1 ]; then
+    echo "ERROR: '$vConfFile' can only have one line"
+    exit 1
+fi
+
+vFile=`cat $vConfFile`
+vFile=`echo $vFile`
+
+if [ ! -f "$vFile" ]; then
+    echo "ERROR: The '$vFile' file to grep in does not exist"
+    exit 1 
+fi
+
+echo "Copying from $vFile..."
+
+vResult=`grep "$1" "$vFile"`
+vNumLines=`echo "$vResult" | wc -l | cut -f 1 -d' '`
+vResult=`echo $vResult | head -n 1`
+
+vKey=`echo $vResult | cut -f 1 -d':'`
+vValue=`echo $vResult | cut -f 2 -d':'`
+vKey=`echo $vKey`
+vValue=`echo $vValue`
+
+if [ $vNumLines != 1 ]; then
+    echo "WARNING: Multiple matches! Only using the first one for '$vKey'"
+fi
+
+echo "Copied value for '$vKey' key!"
+echo -n "$vValue" | xclip -sel clip
