80e08eec0291c6a0afe94a20560a9f1a8105a54c
Alin Tomescu <alinush@mit.edu>
2015-03-06 21:56:08 -0500

    added some error checking in clipboard.sh

--- src/clipboard.sh
+++ src/clipboard.sh
@@ -33,10 +33,27 @@ vResult=`grep "^$1" "$vFile"`
 vNumLines=`echo "$vResult" | wc -l | cut -f 1 -d' '`
 vResult=`echo $vResult | head -n 1`
 
+if [ -z "$vResult" ]; then
+    echo "ERROR: Nothing found for key '$1'"
+    exit 1
+fi
+
 vKey=`echo $vResult | cut -f 1 -d':'`
 vValue=`echo $vResult | cut -f 2 -d':'`
 vKey=`echo $vKey`
 vValue=`echo $vValue`
+
+if [ -z "$vKey" ]; then
+    echo "ERROR: Could not find key '$1' on matched line(s): "
+    echo "$vResult"
+    exit 1
+fi
+
+if [ -z "$vValue" ]; then
+    echo "ERROR: Could not any value for key '$1' on matched line(s): "
+    echo "'$vResult'"
+    exit 1
+fi
 
 if [ $vNumLines != 1 ]; then
     echo "WARNING: Multiple matches! Only using the first one for '$vKey'"
