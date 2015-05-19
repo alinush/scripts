2e793cbc3821d27473997bfda6ff1a68ba255046
Alin Tomescu <alinush@mit.edu>
2015-05-19 08:28:50 -0400

    list-extensions.sh can take dir as parameter

--- src/list-extensions.sh
+++ src/list-extensions.sh
@@ -1,1 +1,6 @@
-find . -type f | perl -ne 'print $1 if m/\.([^.\/]+)$/' | sort -u
+vDir=${1:-`pwd`}
+vDir=`readlink -f $vDir`
+
+echo "Listing extension in directory: $vDir"
+
+find $vDir -type f | perl -ne 'print $1 if m/\.([^.\/]+)$/' | sort -u
