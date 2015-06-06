2c63eb50e1c81ad82e6b4c952213864342b9d20e
Alin Tomescu <alinush@mit.edu>
2015-06-05 22:30:01 -0400

    using exiftool in picsetdate.sh now

--- src/picdates.sh
+++ src/picdates.sh
@@ -3,7 +3,17 @@ which jhead || { echo "ERROR: You need to have 'jhead' installed"; exit 1; }
 OIFS="$IFS"
 IFS=$'\n'
 for file in `find -type f | grep -v Thumbs.db`; do
-    date=`jhead "$file" | grep "Date/Time"`
+    ispic=0
+    nodate=0
+
+    jhead $file 2>&1 >/dev/null && ispic=1
+    date=`jhead "$file" | grep "Date/Time" | cut -d':' -f 2-`
+    [ -z "$date" ] && { nodate=1 ; date="*Date not set"; }
+
+    if [ $ispic -eq 0 ]; then
+        continue
+    fi
+
     echo "$date - $file"
 done | sort -r | uniq
 IFS="$OIFS"
--- src/picsetdate.sh
+++ src/picsetdate.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 which jhead || { echo "ERROR: You need to have 'jhead' installed"; exit 1; }
+which exiftool || { echo "ERROR: You need to have 'exiftool' installed"; exit 1; }
 
 if [ $# -ne 2 ]; then
     echo "Replaces all EXIF dates in picture files that match the"
@@ -17,11 +18,22 @@ newdate=${2:-"2003:07:28-00:00-00"}
 OIFS="$IFS"
 IFS=$'\n'
 for file in `find -type f | grep -v Thumbs.db`; do
+    ispic=0
+    nodate=0
+
+    jhead $file 2>&1 >/dev/null && ispic=1
     date=`jhead "$file" | grep "Date/Time" | cut -d':' -f 2-`
+    [ -z "$date" ] && nodate=1
     matched=`echo $date | grep "$pattern"`
-    if [ -n "$matched" ]; then
+
+    if [ $ispic -eq 0 ]; then
+        continue
+    fi
+
+    if [ -n "$matched" -o $nodate -eq 1 ]; then
         echo "Replacing '$date' date with '$newdate' in file $file"
-        jhead -ts"$newdate" "$file"
+        #jhead -ts"$newdate" "$file" -- does not work when EXIF field is missing
+        exiftool -overwrite_original -DateTimeOriginal="$newdate" "$file"
     else
         echo "Date '$date' did not match '$pattern' pattern in file $file"
     fi
