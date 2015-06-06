9398d319e32f74bb8eca4117716e134fc2204ef5
Alin Tomescu <alinush@mit.edu>
2015-06-05 21:16:42 -0400

    added clip script and fixed bug in list-extensions.sh

--- src/list-extensions.sh
+++ src/list-extensions.sh
@@ -1,6 +1,6 @@
 vDir=${1:-`pwd`}
-vDir=`readlink -f $vDir`
+vDir=`readlink -f "$vDir"`
 
 echo "Listing extension in directory: $vDir"
 
-find $vDir -type f | perl -ne 'print $1 if m/\.([^.\/]+)$/' | sort -u
+find "$vDir" -type f | perl -ne 'print $1 if m/\.([^.\/]+)$/' | sort -u
