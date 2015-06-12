8b1fbe38b2b4df9c46557ab44b48bf06b8b1f1b8
Alin Tomescu <alinush@mit.edu>
2015-06-12 12:52:37 -0400

    help message for find-duplicates.sh

--- src/find-duplicates.sh
+++ src/find-duplicates.sh
@@ -1,3 +1,15 @@
-# use 'uniq -d -w 64' to print duplicates by matching on the first 64
+# use 'uniq -w 64' to print duplicates by matching on the first 64
 # characters only
+#
+# use 'uniq -D' to print ALL duplicate lines
+
+if [ "$1" == "-h" -o "$1" == "--help" ]; then
+    echo "Usage: `basename $0` [OPTIONS]"
+    echo
+    echo "OPTIONS:"
+    echo "   -D, --all-repeated[=delimit-method]    prints all duplicate lines (see 'man uniq')"
+    echo
+    exit
+fi
+
 find . -type f -print0 | xargs -0 sha256sum | sort | uniq $@ -d --check-chars=64
