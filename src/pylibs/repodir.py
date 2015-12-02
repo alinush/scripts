ed9d5c880a72e8df3e90ad071e6642520a85d27d
Alin Tomescu <alinush@mit.edu>
2015-12-01 22:20:39 -0500

    updated repo scripts to deal with missing repos.conf file

--- src/pylibs/repodir.py
+++ src/pylibs/repodir.py
@@ -1,15 +1,16 @@
 import os
 
 HOME =  os.path.expanduser("~")
-REPO_DIR = HOME + '/repos/' # TODO: Remove me
 REPOS_DIRS = [ HOME + '/repos/' ]
+REPOS_CONF = "repos.conf"
 
-with open("repos.conf") as f:
-    extraDirs = [line.strip('\n') for line in f.readlines()]
+if os.path.isfile(REPOS_CONF):
+    with open(REPOS_CONF) as f:
+        extraDirs = [line.strip('\n') for line in f.readlines()]
 
-for i in range(0, len(extraDirs)):
-    length = len(extraDirs[i])
-    if extraDirs[i][length-1] != '/':
-        extraDirs[i] += '/'
+    for i in range(0, len(extraDirs)):
+        length = len(extraDirs[i])
+        if extraDirs[i][length-1] != '/':
+            extraDirs[i] += '/'
 
-REPOS_DIRS.extend(extraDirs)
+    REPOS_DIRS.extend(extraDirs)
