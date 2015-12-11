489689d43524fd72cfaeca228baf41810cca384c
Alin Tomescu <alinush@mit.edu>
2015-12-10 19:17:39 -0500

    added repo or repodir parameter to repo-update.py

--- src/TODO.md
+++ src/TODO.md
@@ -5,17 +5,20 @@ TODO
 ----------------
 
  - Handle repositories with submodules (update submodules as well)
- - Add `<repo_name>` argument to specify repo (just updates that repo)
  - Add `--stash` parameter to stash changes and pull (for Git repos)
+ - [DONE] Add `<repo_name>` argument to specify repo (just updates that repo)
+ - [DONE] Add `<dir_name>` argument to specify repository directory (dir where repos are) to update all repos in
  - [DONE] Add config-file that specifies extra repo directories to work with
 
 `encrypt.sh` and `decrypt.sh`
 -----------------------------
+
  - Remove .enc extension of encrypted files, if present.
  - Allow multiple inputs to `encrypt.sh` (and `decrypt.sh`?) and maybe tar them up and encrypt the tar
 
 Others
 ------
+
  + [DONE] Write a `hashdir` script that recursively hashes a directory with SHA256
  + Fix `gacp` to check for the last commit message, and if it's the same as the current one, it should not commit nor push.
  + Write a script that renames "Bad Linux File Name" into "better-linux-file-name"
--- src/repo-update.py
+++ src/repo-update.py
@@ -2,14 +2,13 @@
 
 import os
 import re
+import click    # command line arguments
 import subprocess
 from multiprocessing.pool import ThreadPool
 from pylibs.colors import *
 from pylibs.repodir import REPOS_DIRS
 
 pool = ThreadPool(16)
-procs = []
-
 
 def create_proc(cmd, d):
     """ This runs in a separate thread. """
@@ -49,33 +48,84 @@ def svn_repo_updated(res, d):
     print cTxtGreen + "[SVN] " + cTxtDefault + d + " -> " + status + cTxtDefault
 
 
-def lambda_factory(d):
-    return lambda res: git_repo_updated(res, d)
+def lambda_factory(d, func):
+    return lambda res: func(res, d)
 
-print "Looking at repos in '%s' ...\n" % REPOS_DIRS
+def is_repo(d):
+    isRepo = True
+    repo_updated = None
+    cmd = None
+    t = "unknown"
 
-for reposDir in REPOS_DIRS:
-    for d in os.listdir(reposDir):
-        os.chdir(reposDir + d)
-        if os.path.isdir(".git"):
-            cmd = ["git", "pull", "--rebase"]
-            t = "git"
-            repo_updated = lambda_factory(d)
-        elif os.path.isdir(".svn"):
-            cmd = ["svn", "update"]
-            t = "svn"
-        else:
-            print d + " -> " + cTxtBoldRed + "(not a repo)" + cTxtDefault
-            continue
-        os.chdir(reposDir)
+    cwd = os.getcwd()
+    os.chdir(d)
 
-        procs.append(pool.apply_async(
+    if os.path.isdir(".git"):
+        cmd = ["git", "pull", "--rebase"]
+        t = "git"
+        repo_updated = lambda_factory(d, git_repo_updated)
+    elif os.path.isdir(".svn"):
+        cmd = ["svn", "update"]
+        t = "svn"
+        repo_updated = lambda_factory(d, svn_repo_updated)
+    else:
+        isRepo = False
+    
+    os.chdir(cwd)
+    return (isRepo, cmd, t, repo_updated)
+
+def update_repo(pool, d):
+    isRepo, cmd, t, repo_updated = is_repo(d)
+
+    if isRepo:
+        pool.apply_async(
             func=create_proc,
-            args=(cmd, reposDir + d),
+            args=(cmd, d),
             callback=repo_updated
-            )
         )
+    else:
+        print d + " -> " + cTxtBoldRed + "(not a repo)" + cTxtDefault
 
-# Close the pool and wait for each running task to complete
-pool.close()
-pool.join()
+def update_repos_dir(pool, reposDir):
+    cwd = os.getcwd()
+    os.chdir(reposDir)
+    for d in os.listdir(reposDir):
+        update_repo(pool, d)
+    os.chdir(cwd)
+
+
+@click.command()
+@click.argument('repo_or_dir_name', required=False, type=click.STRING)
+def main(repo_or_dir_name):
+    if repo_or_dir_name is not None:
+        isRepo, _, _, _ = is_repo(repo_or_dir_name)
+        if isRepo:
+            print "Looking at specified repo '%s' ...\n" % repo_or_dir_name
+            update_repo(pool, repo_or_dir_name)
+        else:
+            parent = repo_or_dir_name
+            while True:
+                parent = os.path.abspath(os.path.join(parent, os.pardir))
+                isRepo, _, _, _ = is_repo(parent)
+                if isRepo:
+                    print "Looking at parent repo in specified directory '%s' ...\n" % repo_or_dir_name
+                    update_repo(pool, parent)
+                    break
+
+                if parent == '/':
+                    break
+
+            if parent == '/':
+                print "Looking at repos in specified directory '%s' ...\n" % repo_or_dir_name
+                update_repos_dir(pool, repo_or_dir_name)
+    else:
+        print "Looking at repos in preconfigured directories '%s' ...\n" % REPOS_DIRS
+        for reposDir in REPOS_DIRS:
+            update_repos_dir(pool, reposDir)
+
+    # Close the pool and wait for each running task to complete
+    pool.close()
+    pool.join()
+
+if __name__ == '__main__':
+    main()
