1b765e852dab31f82f3160deb6aff5d0be870898
Alin Tomescu <alinush@mit.edu>
2016-09-30 17:45:17 -0400

    updated find-duplicates.sh to use guniq on OS X

--- src/find-duplicates.sh
+++ src/find-duplicates.sh
@@ -12,4 +12,8 @@ if [ "$1" == "-h" -o "$1" == "--help" ]; then
     exit
 fi
 
-find . -type f -print0 | xargs -0 sha256sum | sort | uniq $@ -d --check-chars=64
+if [ "$(uname -s)" = "Darwin" ]; then
+    find . -type f -print0 | xargs -0 gsha256sum | sort | guniq $@ -dD --check-chars=64
+else
+    find . -type f -print0 | xargs -0 sha256sum | sort | uniq $@ -d --check-chars=64
+fi
