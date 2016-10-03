2210d8db465114c5a39d87f7404a196f6d6f3eff
Alin Tomescu <alinush@mit.edu>
2016-10-03 09:07:25 -0400

    pdf-merge: make sure dest file does not exit

--- src/pdf-merge.sh
+++ src/pdf-merge.sh
@@ -1,2 +1,5 @@
+[ -f "all.pdf" ] && { echo "ERROR: Destination file 'all.pdf' already exists. Will not overwrite, so please move it."; exit 1; }
+
 echo "Merging documents together: $@"
+
 gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile=all.pdf $@
