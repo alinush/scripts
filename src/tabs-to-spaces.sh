7d7e844a42b2c5cc8ee2b29caac1addd0cb7165a
Alin Tomescu <alinush@mit.edu>
2017-10-13 17:11:52 -0400

    tabs-to-spaces.sh for individual files

--- src/tabs-to-spaces.sh
+++ src/tabs-to-spaces.sh
@@ -1,17 +1,21 @@
 if [ $# -eq 0 ]; then
-    echo "Converts all leading tabs to 4 spaces in all the files from the specified directory (recursively)"
+    echo "Converts all leading tabs to 4 spaces in all the files from the specified file or directory (recursively)"
     echo 
-    echo "Usage: $0 <directory>"
+    echo "Usage: $0 <directory-or-file>"
     exit 0
 fi
 
-if [ ! -d "$1" ]; then
-    echo "ERROR: '$1' is not a directory."
-    exit 1
+cmd=gsed
+
+! which $cmd &>/dev/null && cmd=sed
+
+if [ -d "$1" ]; then
+    vTargetDir=`readlink -f $1`
+
+    #find $vTargetDir -type f -exec sed -i 's/\t/    /g' {} \;
+    #find $vTargetDir -type f ! -name Makefile -exec sh -c 'tFile=`mktemp`; expand --initial --tabs=4 {} >$tFile && mv $tFile {};' \;
+    find $vTargetDir -type f ! -name Makefile -exec $cmd -ri ':a;s/^( *)\t/\1    /;ta' {} \;
+else
+    $cmd -ri ':a;s/^( *)\t/\1    /;ta' "$1"
 fi
 
-vTargetDir=`readlink -f $1`
-
-#find $vTargetDir -type f -exec sed -i 's/\t/    /g' {} \;
-#find $vTargetDir -type f ! -name Makefile -exec sh -c 'tFile=`mktemp`; expand --initial --tabs=4 {} >$tFile && mv $tFile {};' \;
-find $vTargetDir -type f ! -name Makefile -exec sed -ri ':a;s/^( *)\t/\1    /;ta' {} \;
