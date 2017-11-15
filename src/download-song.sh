f96d2a22818250cb1405e2378e8c6ae9b463cfde
Alin Tomescu <alinush@mit.edu>
2017-11-14 19:16:44 -0500

    download-song.sh: specify audio format

--- src/download-song.sh
+++ src/download-song.sh
@@ -1,7 +1,9 @@
 if [ $# -lt 1 ]; then
-    echo "Usage: $0 <youtube-url>"
+    echo "Usage: $0 <youtube-url> [audio-format]"
     exit 1
 fi 
 
+audio_format=${2:-'m4a'}
+
 # --embed-thumbnail
-youtube-dl -x "$1" --audio-format best --audio-quality 0 -o "%(title)s.%(ext)s"
+youtube-dl -x "$1" --audio-format "$audio_format" --audio-quality 0 -o "%(title)s.%(ext)s"
