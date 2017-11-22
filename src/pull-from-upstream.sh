32c9fd42a18a87d4699c13085a7b7e28ddd226a8
Alin Tomescu <alinush@mit.edu>
2017-11-22 17:40:58 -0500

    pull-from-upstream.sh: use upstream remote from repo by default

--- src/pull-from-upstream.sh
+++ src/pull-from-upstream.sh
@@ -1,18 +1,31 @@
 # See https://help.github.com/articles/syncing-a-fork/
 
-remote=$1
+num_args=0
 
-if [ $# -lt 1 ]; then
+# If there's no upstream remote configured in the repo, ask for an argument!
+if [ -z "`git remote | grep upstream`" ]; then
+    num_args=1
+fi
+
+if [ $# -lt $num_args ]; then
     name=`basename $0`
-    echo "Usage: $name <git-repository-remote>"
+    echo "Usage: $name [<git-repository-remote>]"
+    echo 
+    echo "If there's already an 'upstream' remote configured in the repo, just uses that and ignores argument"
     echo 
     echo "Example: $name https://github.com/scipr-lab/libfqfft.git"
     echo
     exit 1
 fi
 
-if ! git remote add upstream "$remote"; then
-    echo "NOTE: 'upstream' remote already exists. Proceeding..."
+
+remote=$1
+
+# If we were given a remote, try adding it!
+if [ -n "$remote" ]; then
+    if ! git remote add upstream "$remote"; then
+        echo "NOTE: 'upstream' remote already exists. Proceeding..."
+    fi
 fi
 
 git fetch upstream
