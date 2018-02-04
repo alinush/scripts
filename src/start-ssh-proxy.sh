389ba4781058fa8044a19652f3db20d9e7d5ab0e
Alin Tomescu <alinush@mit.edu>
2018-02-04 06:11:25 +0100

    check SSH key is not password-protected before going into daemon mode

--- src/start-ssh-proxy.sh
+++ src/start-ssh-proxy.sh
@@ -21,7 +21,17 @@ vBackground=
 if [ "$3" == "-b" ]; then
     echo
     echo "Running in background as a daemon..."
-    vBackground="-f"
+    if [ -f ~/.ssh/id_rsa ]; then
+        if head -n 2 ~/.ssh/id_rsa | tail -n 1 | grep "ENCRYPTED" &>/dev/null; then
+            echo "ERROR: ~/.ssh/id_rsa is password protected, so cannot run in background mode."
+            exit 1
+        else
+            vBackground="-f"
+        fi
+    else
+        echo "ERROR: Need to ensure SSH secret key is not password-protected but no key file at ~/.ssh/id_rsa"
+        exit 1
+    fi
 fi
 
 echo
@@ -34,9 +44,18 @@ echo
 # -q uses quiet mode
 # -N means no commands will be sent to SSH server
 # $vServer specifies user@server pair
-set -x
-ssh $vBackground    \
+#set -x
+#ssh                 \
+#    $vBackground    \
+#    -D $vPort       \
+#    -q              \
+#    -N              \
+#    $vServer        \
+
+# WARNING: Do not move $vBackground up because autossh will use it rather than ssh
+autossh -M 20000    \
     -D $vPort       \
     -q              \
     -N              \
-    $vServer        \
+    $vBackground    \
+    $vServer
