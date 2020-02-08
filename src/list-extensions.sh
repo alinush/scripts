f40b445de900b9d20608195587915f75adf1399e
Alin Tomescu <alinush@mit.edu>
2020-02-07 16:42:34 -0800

    list-extensions.sh works non-recursively too now

--- src/list-extensions.sh
+++ src/list-extensions.sh
@@ -1,6 +1,23 @@
+#!/bin/bash
+
+if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+    echo "Usage: $0 [-r] [directory]"
+    exit
+fi
+
+recursive=0
+if [ "$1" = "-r" ]; then
+    recursive=1
+    shift
+fi
+
 vDir=${1:-`pwd`}
 vDir=$(cd "$vDir"; pwd -P)
 
-echo "Listing extension in directory: $vDir"
-
-find "$vDir" -type f | perl -ne 'print $1 if m/\.([^.\/]+)$/' | sort -u
+if [ $recursive -eq 0 ]; then
+    echo "Listing extensions in this directory only: $vDir"
+    find "$vDir" -type f -maxdepth 1 | perl -ne 'print $1 if m/\.([^.\/]+)$/' | sort -u
+else
+    echo "Listing extensions in this directory and its subdirs: $vDir"
+    find "$vDir" -type f | perl -ne 'print $1 if m/\.([^.\/]+)$/' | sort -u
+fi
